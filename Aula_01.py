d503fb201d27a4ac8862a427f7b06d0b61972ec1
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-08-25 08:48:58 -0300

    Commitando certo

---
+++ Aula_01.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+"""
+print(np.__version__)
+
+a1D_1 = np.array([1,2,3,4])
+a1D_2 = np.array([5,6,7,8])
+a1D = np.concatenate((a1D_1,a1D_2))
+
+lista = [1,2,3,4]
+tupla = (1,2,3,4)
+
+
+print(a1D_1[3])
+print(a1D_2[0])
+print(a1D[4])
+print(a1D[2:6:2])
+print("Tipo de um Array NumPy: ", type(a1D))
+print(f"Dados sobre a1D:\nDimensão: {a1D.ndim}\nTamanho: {a1D.size}\nTipo: {a1D.dtype}\nTamanho do Elemento: {a1D.itemsize}")
+
+a2D = np.array([[1.0, 2.0], [3.0, 4.0]])
+print(a2D[0, 0]) #Pega o elemento da primeira linha e da primeira coluna.
+print(type(a2D))
+print(a2D[::-1,::-1])
+print(f"Dados sobre a2D:\nDimensão: {a2D.ndim}\nTamanho: {a2D.size}\nTipo: {a2D.dtype}\nTamanho do Elemento: {a2D.itemsize}")
+print(f"forma do a2D: {a2D.shape}")
+"""
+
+"""
+a3D = np.array([[[1,2], [3,4]], [[1,2], [3,4]]], dtype=np.float64)
+print(a3D[0, 0, 0]) #Pega o elemento da primeira linha e da primeira coluna.
+print(f"Dados sobre a3D:\nDimensão: {a3D.ndim}\nTamanho: {a3D.size}\nTipo: {a3D.dtype}\nTamanho do Elemento: {a3D.itemsize}")
+print(f"forma do a3D: {a3D.shape}")
+"""
